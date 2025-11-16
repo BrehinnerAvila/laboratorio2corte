206d0a7273c1cd177dce8dd0bf3414216732df35
brehiner yesid avila peña <brehinneravila125@unisangil.edu.co>
2025-11-16 14:45:24 -0500

    codigo actulizados

--- simulad_banco.py
+++ simulad_banco.py
@@ -1,9 +1,7 @@
-
 class CuentaBancaria:
     def __init__(self, titular, saldo):
         self.titular = titular
         self.saldo = saldo
-
     # Método para depositar
     def depositar(self, cantidad):
         if cantidad > 0:
@@ -11,7 +9,6 @@ class CuentaBancaria:
             print(f"{self.titular} ha depositado ${cantidad}. Nuevo saldo: ${self.saldo}")
         else:
             print("La cantidad debe ser positiva.")
-
     # Método para retirar
     def retirar(self, cantidad):
         if cantidad > 0:
@@ -22,23 +19,17 @@ class CuentaBancaria:
                 print("Fondos insuficientes.")
         else:
             print("La cantidad debe ser positiva.")
-
     # Método para consultar el saldo
     def consultar_saldo(self):
         print(f"Saldo actual de {self.titular}: {self.saldo}")
-
-
 # Función que procesa la fila de clientes
 def procesar_fila(fila):
     print("\nProcesando clientes en la fila\n")
     for cliente in fila:  # cada cliente trae una acción
         nombre, accion, monto = cliente
-
         # Crear una cuenta para cada cliente (simple para la simulación)
         cuenta = CuentaBancaria(nombre, 10000)  # saldo inicial fijo
-
         print(f"Atendiendo a: {nombre}")
-
         if accion == "deposito":
             cuenta.depositar(monto)
         elif accion == "retiro":
@@ -47,11 +38,9 @@ def procesar_fila(fila):
             cuenta.consultar_saldo()
         else:
             print("Acción no válida.")
-
         print()  # salto de línea
-#    PRGRAMA PRINCIPAL     
-# Fila simulada (cola)
-# Cada cliente es: [nombre, acción, monto]
+                                #    PRGRAMA PRINCIPAL     
+#lista de  nombre, acción, monto 
 fila_clientes = [
     ["Juan", "deposito", 5000],
     ["Maria", "retiro", 3000],
